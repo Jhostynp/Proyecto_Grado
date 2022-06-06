056bdf6005a49a2fd404b2751cbb5e934e6cc4a2
Jhostynp <pjhostyn1979@gmail.com>
2022-06-06 11:21:01 -0500

    Version Final 4.0

--- database/migrations/2030_04_29_160014_create_factura_detalle_table.php
+++ database/migrations/2030_04_29_160014_create_factura_detalle_table.php
@@ -15,10 +15,7 @@ class CreateFacturaDetalleTable extends Migration
     {
         Schema::create('factura_detalle', function (Blueprint $table) {
             $table->id('fad_id');
-<<<<<<< HEAD
             $table->foreignid('fac_id')->references('fac_id')->on('facturas');
-=======
->>>>>>> 5967af72b22685d6b2935f53f01dd7e6cf7bf701
             $table->foreignid('prod_id')->references('prod_id')->on('productos');
             $table->integer('fad_cantidad');
             $table->float('fad_vu');
